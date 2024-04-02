de214d2f5c78481c888be2fbe7bb0860c7815b6d
Lukas Jacomin <lukasjacomin@gmail.com>
2024-04-02 18:12:28 -0300

    refactor: logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,22 +1,45 @@
-export function logStart(spacing: number, ...args: unknown[]): void {
-  logSymbol("┌", spacing, ...args);
+const START_SYMBOL = "┌";
+const CONTINUE_SYMBOL = "│";
+const LOGG_SYMBOL = "├";
+const END_SYMBOL = "└";
+const SPACING_SYMBOL = "─";
+
+export function loggStart(spacing: number, ...args: unknown[]): void {
+  loggSymbol(START_SYMBOL, spacing, ...args);
 }
 
-export function logEnd(spacing: number, ...args: unknown[]): void {
-  logSymbol("└", spacing, ...args);
+export function loggEnd(spacing: number, ...args: unknown[]): void {
+  loggSymbol(END_SYMBOL, spacing, ...args);
 }
 
 export function logg(spacing: number, ...args: unknown[]): void {
-  logSymbol("├", spacing, ...args);
+  loggSymbol(LOGG_SYMBOL, spacing, ...args);
 }
 
-export function logContinue(spacing: number, ...args: unknown[]): void {
-  logSymbol("│", spacing, ...args);
+export function loggContinue(spacing: number, ...args: unknown[]): void {
+  loggSymbol(CONTINUE_SYMBOL, spacing, ...args);
 }
 
-export function logSymbol(symbol: string, spacing: number, ...args: unknown[]): void {
+export function loggMultiLine(spacing: number, ...args: unknown[]): void {
+  for (const arg of args) {
+    if (typeof arg === "string") {
+      const lines = arg.split("\n");
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i].trim();
+        if (i === lines.length - 1 && !line.length) continue;
+        if (line.length) {
+          logg(spacing, line);
+        } else {
+          loggContinue(1);
+        }
+      }
+    }
+  }
+}
+
+function loggSymbol(symbol: string, spacing: number, ...args: unknown[]): void {
   if (spacing > 0) {
-    const spaces = `${symbol}${new Array(spacing).join("─")}`;
+    const spaces = `${symbol}${new Array(spacing).join(SPACING_SYMBOL)}`;
     console.log(spaces, ...args);
   } else {
     console.log(...args);
