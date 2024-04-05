4d9fe7b6c381389c0dd59ffa4775b2fdd9765278
Lukas Jacomin <lukasjacomin@gmail.com>
2024-04-05 00:11:11 -0300

    feat: identityFile validation

--- src/schemas/server.schema.ts
+++ src/schemas/server.schema.ts
@@ -1,3 +1,4 @@
+import { existsSync } from "fs";
 import { z } from "zod";
 
 export const ServerSchema = z.object({
@@ -6,7 +7,17 @@ export const ServerSchema = z.object({
   port: z.number().int().positive().default(22),
   username: z.string(),
   password: z.string().optional(),
-  identityFile: z.union([z.string(), z.instanceof(Buffer)]).optional(),
+  identityFile: z
+    .union([z.string(), z.instanceof(Buffer)])
+    .optional()
+    .refine(
+      (filePath) => {
+        if (!filePath || filePath instanceof Buffer) return true;
+        if (!existsSync(filePath)) return false;
+        return true;
+      },
+      (filePath) => ({ message: `Identify file not found at '${filePath}'.` }),
+    ),
   disabled: z.boolean().optional().default(false),
   timeout: z.number().optional().default(10000),
 });
